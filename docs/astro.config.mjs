391a22fc2d749a17a535ff58a395953d25df0be9
Pasquale <p.antonante@gmail.com>
2024-01-04 00:37:19 -0500

    Draft of the EnsembleMetric page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -9,75 +9,73 @@ export default defineConfig({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
   },
-	integrations: [
-		starlight({
-			title: 'Continuous Eval',
-			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4, },
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			],
-			social: {
-				github: 'https://github.com/relari-ai/continuous-eval/tree/main',
-			},
-			sidebar: [
-				{
-					label: 'Getting Started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Introduction', link: '/getting-started/introduction/'},
-						{ label: 'Installation', link: '/getting-started/installation/' },
-						{ label: 'Quick Start', link: '/getting-started/quickstart/' },
-					],
-				},
-				{
-					label: 'Metrics',
-					items: [
-						{ label: 'Overview', link: '/metrics/overview/' },
-						{
-							label: 'Retrieval',
-							autogenerate: { directory: '/metrics/Retrieval/' }
-						},
-						{
-							label: 'Generation',
-							items: [
-								{
-									label: 'Deterministic',
-									autogenerate: { directory: '/metrics/Generation/Deterministic/' }
-								},
-								{
-									label: 'Semantic',
-									items: [
-										{ label: 'DeBERTa Answer Scores', link: '/metrics/generation/semantic/deberta_answer_scores/' },
-										{ label: 'BERT Answer Similarity', link: '/metrics/generation/semantic/bert_answer_similarity/' },
-										{ label: 'BERT Answer Relevance', link: '/metrics/generation/semantic/bert_answer_relevance/' },
-									]
-								},
-								{
-									label: 'LLM-Based',
-									autogenerate: { directory: '/metrics/Generation/LLM-Based/' }
-								},
-							]
-						},
-					],
-				},
-				{
-					label: 'Datasets',
-					autogenerate: { directory: 'evaluators' },
-				},
-				{
-					label: 'Metric Ensembling',
-					items: [
-						{ label: 'Conformal Prediction', link: '/classification/conformal_prediction/' },
-						{ label: 'Classification', link: '/classification/classifier/' },
-					],
-				},
-
-        		{
-					label: 'Examples',
-					autogenerate: { directory: 'examples' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Continuous Eval',
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4, },
+      customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
+      social: {
+        github: 'https://github.com/relari-ai/continuous-eval/tree/main',
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Introduction', link: '/getting-started/introduction/' },
+            { label: 'Installation', link: '/getting-started/installation/' },
+            { label: 'Quick Start', link: '/getting-started/quickstart/' },
+          ],
+        },
+        {
+          label: 'Metrics',
+          items: [
+            { label: 'Overview', link: '/metrics/overview/' },
+            {
+              label: 'Retrieval',
+              autogenerate: { directory: '/metrics/Retrieval/' }
+            },
+            {
+              label: 'Generation',
+              items: [
+                {
+                  label: 'Deterministic',
+                  autogenerate: { directory: '/metrics/Generation/Deterministic/' }
+                },
+                {
+                  label: 'Semantic',
+                  items: [
+                    { label: 'DeBERTa Answer Scores', link: '/metrics/generation/semantic/deberta_answer_scores/' },
+                    { label: 'BERT Answer Similarity', link: '/metrics/generation/semantic/bert_answer_similarity/' },
+                    { label: 'BERT Answer Relevance', link: '/metrics/generation/semantic/bert_answer_relevance/' },
+                  ]
+                },
+                {
+                  label: 'LLM-Based',
+                  autogenerate: { directory: '/metrics/Generation/LLM-Based/' }
+                },
+              ]
+            },
+          ],
+        },
+        {
+          label: 'Datasets',
+          autogenerate: { directory: 'evaluators' },
+        },
+        {
+          label: 'Metric Ensembling',
+          items: [
+            { label: 'Classification', link: '/classification/classifier/' },
+          ],
+        },
+        {
+          label: 'Examples',
+          autogenerate: { directory: 'examples' },
+        },
+      ],
+    }),
+  ],
 });
