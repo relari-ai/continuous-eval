affe539383bf7c6b4de366015fb87f18bf3bd963
Pasquale Antonante <p.antonante@gmail.com>
2024-02-18 21:33:18 -0800

    Docs version selector

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -5,3 +5,12 @@ export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
+
+export const base = '/';
+
+export const versions = [
+  ['v0.3', 'v0.3 (canary)'],
+  ['v0.2', 'v0.2 (latest)'],
+];
+
+export const defaultVersion = 'v0.3';
